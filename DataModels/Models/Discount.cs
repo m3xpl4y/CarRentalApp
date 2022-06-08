2a6dffd210aa3c8f4309b5a08611f99fbb3312e3
m3xpl4y <m3xpl4y@gmail.com>
2022-06-08 13:31:30 +0200

    added models

---
+++ DataModels/Models/Discount.cs
@@ -0,0 +1,10 @@
+﻿namespace DataModels.Models;
+public class Discount
+{
+    public int Id { get; set; }
+    [MaxLength(100, ErrorMessage = "Maximal Länge überschritten")]
+    [Display(Name = "Bezeichnung")]
+    public string Name { get; set; } = string.Empty;
+    [Display(Name = "Prozente")]
+    public double Percentage { get; set; }
+}
---
+++ DataModels/Models/Price.cs
@@ -0,0 +1,10 @@
+﻿namespace DataModels.Models;
+public class Price
+{
+    public int Id { get; set; }
+    [MaxLength(100, ErrorMessage = "Maximal Länge überschritten")]
+    [Display(Name = "Bezeichnung")]
+    public string Name { get; set; } = string.Empty;
+    [Display(Name = "Betrag")]
+    public decimal Amount { get; set; }
+}
