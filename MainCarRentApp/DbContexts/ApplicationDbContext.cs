83f5af4ae188b7e6b4e66d1e4546fbff192a6397
m3xpl4y <m3xpl4y@gmail.com>
2022-06-10 18:16:50 +0200

    added WPF-Project

---
+++ MainCarRentApp/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MainCarRentApp.DbContexts;
+public class ApplicationDbContext : DbContext
+{
+}
