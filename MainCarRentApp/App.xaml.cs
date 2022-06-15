81f8a39466b58b003c7f82e111c8a00902167077
m3xpl4y <m3xpl4y@gmail.com>
2022-06-15 18:25:47 +0200

    updated some stuff to test

--- MainCarRentApp/App.xaml.cs
+++ MainCarRentApp/App.xaml.cs
@@ -1,11 +1,5 @@
 ﻿using MainCarRentApp.DbContexts;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace MainCarRentApp
@@ -16,6 +10,7 @@ namespace MainCarRentApp
     public partial class App : Application
     {
         private const string CONNECTION_STRING = "Data Source = CarRentSystemDB.db";
+        //private const string CONNECTION_STRING = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CarRentSystemDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         protected override void OnStartup(StartupEventArgs e)
         {
             DbContextOptions options = new DbContextOptionsBuilder().UseSqlite(CONNECTION_STRING).Options;
--- MainCarRentApp/DbContexts/ApplicationDbContext.cs
+++ MainCarRentApp/DbContexts/ApplicationDbContext.cs
@@ -10,4 +10,9 @@ public class ApplicationDbContext : DbContext
 
     }
     public DbSet<Client> Clients { get; set; }
+    public DbSet<ClientAddress> ClientAddress { get; set; }
+    public DbSet<ClientDriverLicense> ClientDriverLicense { get; set; }
+    public DbSet<ClientInformation> ClientInformation { get; set; }
+    public DbSet<ClientPassport> ClientPassport { get; set; }
+    public DbSet<ClientPersonalId> ClientPersonalId { get; set; }
 }
