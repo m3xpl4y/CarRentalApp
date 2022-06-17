d9923fa711f06601f8265044cac94f92dd57cfb0
m3xpl4y <m3xpl4y@gmail.com>
2022-06-18 00:25:23 +0200

    added IRepository

---
+++ ICarRental.IMain/IRepository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace ICarRental.IMain.IRepository
+{
+    public interface IRepository<T1, T2> where T1 : class
+    {
+        Task<List<T1>> GetAll();
+        Task<T1> GetbyId(T2 Id);
+        Task Create(T1 model);
+        Task Update(T1 model);
+        Task Delete(T1 model);
+
+    }
+}
